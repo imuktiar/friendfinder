c08cc3bee027d49138889b199995acba542f1140
Muktiar Idrees <imuktiar@hotmail.com>
2019-03-19 16:14:31 -0400

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		name: "damus",
+		photo: "https://steeshes.files.wordpress.com/2012/12/chappelles-show-negrodamus-jpg.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		name: "Ashy Larry",
+		photo: "https://tenor.com/view/dave-chappelle-ashy-larry-gif-3557506",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		name: "Crack Dave",
+		photo: "https://tenor.com/view/chappelle-dave-crack-gif-5622197",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		name: "Rick James",
+		photo: "https://tenor.com/view/dave-chappelle-rick-james-come-here-inviting-in-gif-4104398",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		name: "Prince",
+		photo: "https://tenor.com/view/squad-up-dave-chappelle-assemble-gif-13752116",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
